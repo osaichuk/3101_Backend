fc50f6a60b4c56e7712350b04c137cea54fa0291
osaichuk <oleksandr.saichuk@gmail.com>
2018-03-14 12:51:42 +0200

    correct task 06

--- hw/functions_forms_tasks/06/06.php
+++ hw/functions_forms_tasks/06/06.php
@@ -9,24 +9,6 @@ function requestFiles($key)
     return null;
 }
 
-function requestPost($key)
-{
-    if (isset($_POST[$key])) {
-        return $_POST[$key];
-    }
-
-    return null;
-}
-
-function requestGet($key)
-{
-    if (isset($_GET[$key])) {
-        return $_GET[$key];
-    }
-
-    return null;
-}
-
 function validateForm($image)
 {
     if (!$image){
